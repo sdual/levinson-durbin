b6f1476568bd22602880b415a105595022e42e63
stenoritama <shr.nrht.0623@gmail.com>
2018-03-04 21:04:19 +0900

    fix main

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import numpy as np
 
 from ld import LevinsonDurbinRecursion
 
-toeplitz_matrix_elements = np.array([
-    1.0, 2.0, 3.0, 4.0, 5.0, 6.0
-])
+def main():
+    toeplitz_matrix_elements = np.array([
+        1.0, 2.0, 3.0, 4.0, 5.0, 6.0
+    ])
 
-ld = LevinsonDurbinRecursion(toeplitz_matrix_elements)
-solutions = ld.solve()
-print(solutions)
+    ld = LevinsonDurbinRecursion(toeplitz_matrix_elements)
+    solutions = ld.solve()
+    print(solutions)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
